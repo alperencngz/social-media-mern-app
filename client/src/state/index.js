bd53d9592d26f86541418e3170222840545950cc
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-08-09 09:43:37 +0300

    Color, Theme, Dark Mode and Styling Setup
    I set up the neccesarry configurations to use MUI

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -19,13 +19,14 @@ export const authSlice = createSlice({
       state.token = action.payload.token;
     },
     setLogout: (state) => {
-      (state.user = null), (state.token = null);
+      state.user = null;
+      state.token = null;
     },
     setFriends: (state, action) => {
       if (state.user) {
         state.user.friends = action.payload.friends;
       } else {
-        console.error("No Friends :(");
+        console.error("user friends non-existent :(");
       }
     },
     setPosts: (state, action) => {
@@ -33,7 +34,7 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
