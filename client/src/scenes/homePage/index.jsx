f2e4c926a31a18dabbc2c7e564730385c200e47a
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-08-14 08:47:03 +0300

    AdvertWidget and FriendListWidget
    Added 2 last widgets one for displaying advertisements one for displaying friendlist of current user. The FriendListWidget will display the friends of the user in the visited page.

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
@@ -22,7 +24,6 @@ const HomePage = () => {
         <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
-
         <Box
           flexBasis={isNonMobileScreen ? "42%" : undefined}
           mt={isNonMobileScreen ? undefined : "2rem"}
@@ -30,8 +31,13 @@ const HomePage = () => {
           <MyPostWidget picturePath={picturePath} />
           <PostsWidget userId={_id} />
         </Box>
-
-        {isNonMobileScreen && <Box flexBasis={"26%"}></Box>}
+        {isNonMobileScreen && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
       </Box>
     </Box>
   );
